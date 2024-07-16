1a0ccafc29e993693881917127d0331827c4a69f
mh.lee <mh.lee239@gmail.com>
2024-07-16 21:58:14 +0900

    #[Crypto] - Modified Routes - Add Chart,Price Components

--- src/Router.jsx
+++ src/Router.jsx
@@ -8,18 +8,43 @@ import {
 import App from "./App.jsx";
 import Coins from "./routes/Coins.jsx";
 import Coin from "./routes/Coin.jsx";
+import Chart from "./routes/Chart.jsx";
+import Price from "./routes/Price.jsx";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Coins />,
-  },
-  {
-    path: "/:coinId",
-    element: <Coin />,
-  },
-]);
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <Coins />,
+//   },
+//   {
+//     path: "/:coinId",
+//     element: <Coin />,
+//   },
+// ]);
 
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Coins />,
+    },
+    {
+      path: "/:coinId",
+      element: <Coin />,
+      children: [
+        {
+          path: "chart",
+          element: <Chart />,
+        },
+        {
+          path: "price",
+          element: <Price />,
+        },
+      ],
+    },
+  ],
+  { basename: "/" },
+);
 function Router() {
   return <RouterProvider router={router} />;
 }
--- src/routes/Coin.jsx
+++ src/routes/Coin.jsx
@@ -1,30 +1,155 @@
-import { useLocation, useParams } from "react-router-dom";
+import {
+  Link,
+  Outlet,
+  Route,
+  Routes,
+  useLocation,
+  useMatch,
+  useParams,
+} from "react-router-dom";
 import styled from "styled-components";
+import { useEffect, useState } from "react";
+import Price from "./Price.jsx";
+import Chart from "./Chart.jsx";
+
+const Title = styled.h1`
+  font-size: 48px;
+  color: ${(props) => props.theme.accentColor};
+`;
+const Loader = styled.span`
+  text-align: center;
+  display: block;
+`;
 const Container = styled.div`
-  padding: 0 20px;
+  padding: 0px 20px;
   max-width: 480px;
   margin: 0 auto;
 `;
-
 const Header = styled.header`
   height: 15vh;
   display: flex;
   justify-content: center;
   align-items: center;
 `;
-const Title = styled.h1`
-  font-size: 48px;
-  color: ${(props) => props.theme.accentColor};
+
+const Overview = styled.div`
+  display: flex;
+  justify-content: space-between;
+  background-color: rgba(0, 0, 0, 0.5);
+  padding: 10px 20px;
+  border-radius: 10px;
+`;
+const OverviewItem = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  span:first-child {
+    font-size: 10px;
+    font-weight: 400;
+    text-transform: uppercase;
+    margin-bottom: 5px;
+  }
+`;
+const Description = styled.p`
+  margin: 20px 0;
+`;
+const Tabs = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  margin: 25px 0px;
+  gap: 10px;
+`;
+
+const Tab = styled.span`
+  text-align: center;
+  text-transform: uppercase;
+  font-size: 12px;
+  font-weight: 400;
+  background-color: rgba(0, 0, 0, 0.5);
+  padding: 7px 0px;
+  border-radius: 10px;
+  color: ${(props) =>
+    props.isActive ? props.theme.accentColor : props.theme.textColor};
+
+  a {
+    display: block;
+  }
 `;
 export default function Coin() {
-  const location = useLocation();
+  const [loading, setLoading] = useState(true);
+  const { state } = useLocation();
   const { coinId } = useParams();
-  const { coin } = location.state || {};
+  const [info, setInfo] = useState({});
+  const [price, setPrice] = useState({});
+  const priceMatch = useMatch("/:coinId/price");
+  const chartMatch = useMatch("/:coinId/chart");
+  useEffect(() => {
+    (async () => {
+      const infoResponse = await fetch(
+        `https://api.coinpaprika.com/v1/coins/${coinId}`,
+      );
+      const infoData = await infoResponse.json();
+      const priceResponse = await fetch(
+        `https://api.coinpaprika.com/v1/tickers/${coinId}`,
+      );
+      const priceData = await priceResponse.json();
+      setInfo(infoData);
+      setPrice(priceData);
+      setLoading(false);
+    })();
+  }, []);
   return (
     <Container>
       <Header>
-        <Title>{coin?.name ?? "Loading..."}</Title>
+        <Title>
+          {state?.name ? state.name : loading ? "Loading..." : info?.name}
+        </Title>
       </Header>
+      {loading ? (
+        <Loader>Loading...</Loader>
+      ) : (
+        <>
+          <Overview>
+            <OverviewItem>
+              <span>Rank:</span>
+              <span>{info?.rank}</span>
+            </OverviewItem>
+            <OverviewItem>
+              <span>Symbol:</span>
+              <span>${info?.symbol}</span>
+            </OverviewItem>
+            <OverviewItem>
+              <span>Open Source:</span>
+              <span>{info?.open_source ? "Yes" : "No"}</span>
+            </OverviewItem>
+          </Overview>
+          <Description>{info?.description}</Description>
+          <Overview>
+            <OverviewItem>
+              <span>Total Supply:</span>
+              <span>{price?.total_supply}</span>
+            </OverviewItem>
+            <OverviewItem>
+              <span>Max Supply:</span>
+              <span>{price?.max_supply}</span>
+            </OverviewItem>
+          </Overview>
+          <Tabs>
+            <Tab isActive={chartMatch !== null}>
+              <Link to="chart">Chart</Link>
+            </Tab>
+            <Tab isActive={priceMatch !== null}>
+              <Link to="price">Price</Link>
+            </Tab>
+          </Tabs>
+          <Outlet />
+          {/*<Routes>*/}
+          {/*  <Route path="chart" element={<Chart />} />*/}
+          {/*  <Route path="price" element={<Price />} />*/}
+          {/*</Routes>*/}
+        </>
+      )}
     </Container>
   );
 }
