0016ef6f17dad38deda87176c84affcad1fe91b1
mh.lee <mh.lee239@gmail.com>
2024-10-28 22:32:23 +0900

    devtools 추가

--- src/store/authStore.js
+++ src/store/authStore.js
@@ -2,17 +2,19 @@
 // src/authStore.js
 import { create } from "zustand";
 import { encryptedStorage } from "./encryptedStorage";
+import { devtools } from "zustand/middleware";
+export const useAuthStore = create(
+  devtools((set) => ({
+    user: encryptedStorage.getItem("user") || null, // Retrieve user from encrypted storage on load
 
-export const useAuthStore = create((set) => ({
-  user: encryptedStorage.getItem("user") || null, // Retrieve user from encrypted storage on load
+    setUser: (userData) => {
+      set({ user: userData });
+      encryptedStorage.setItem("user", userData); // Save user data in encrypted storage
+    },
 
-  setUser: (userData) => {
-    set({ user: userData });
-    encryptedStorage.setItem("user", userData); // Save user data in encrypted storage
-  },
-
-  clearAuth: () => {
-    set({ user: null });
-    encryptedStorage.removeItem("user"); // Clear user data from encrypted storage
-  },
-}));
+    clearAuth: () => {
+      set({ user: null });
+      encryptedStorage.removeItem("user"); // Clear user data from encrypted storage
+    },
+  })),
+);
